d50f26863b3034a01f62e8a76112a1e7fe788b4c
chuchilini <vanesalees@gmail.com>
2024-02-15 14:11:40 +0200

    FI-19 Added ERR_NOTREGISTERED

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -10,7 +10,7 @@ class Message;
 
 void cmd_cap(Message &msg, Client *Client);
 int cmd_nick(Message &msg, int client_fd);
-int cmd_pass(Message &msg, Client *Client);
+int cmd_pass(Message &msg, Client *Client, std::string password);
 int cmd_quit(Message &msg, Client *Client);
 int cmd_user(Message &msg, Client *Client);
 int cmd_whois(Message &msg, Client *Client);
