0ff3557a17db821adf8c076a2d9ec6497438f40f
chuchilini <vanesalees@gmail.com>
2024-02-20 11:43:40 +0200

    FI-35 Base channel.hpp channel.cpp ready

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -13,7 +13,6 @@ class Channel
 		std::map<std::string, Client>	_clientList;
 		std::vector<std::string>		_kickedUsers;
 		std::vector<std::string>		_operators;
-		std::string						_operatorPassword;
 		std::string 					_channel;
 		std::string						_channelKey;
 		std::string						_topic;
@@ -24,26 +23,30 @@ class Channel
 		Channel(std::string const &name);
 		~Channel();
 
-
-		const std::string&				getChannelName();
-		const std::string&				getTopic() ;
-		const std::string&				getMode() ;
-		const std::string&				getChannelKey() ;
-		const int&						getUserLimit() ;
-		std::vector<std::string>&		getOperators() ;
-		std::vector<std::string>&		getKickedUsers() ;
+		/*getters and setters*/
 		std::map <std::string, Client>&	getClientList();
-		void							setTopic(std::string& newTopic);
-		void							setChannelKey(std::string password);
-		void							setUserLimit(int limit);
+		// std::vector<std::string>&		getKickedUsers();
+		std::vector<std::string>&		getChannelOperators();
+		const std::string&				getChannelName();
+		const std::string&				getChannelKey();
+		const std::string&				getTopic();
+		const std::string&				getMode();
+		const int&						getUserLimit();
 		void							addToChannel(Client &client);
 		void							removeFromChannel(std::string &nick);
+		// void							addToKicked(std::string &nick);
+		// void							removeFromKicked(std::string &nick);
 		void							addOperator(std::string operatorName);
 		void							removeOperator(std::string operatoName);
-		bool							isOperator(std::string &operatorName);
+		void							setChannelKey(std::string password);
+		void							setTopic(std::string& newTopic);
 		void							addMode(std::string const mode);
 		void							removeMode(std::string const mode);
+		void							setUserLimit(int limit);
+
+		/*helping functions*/
+		bool							isAlreadyInChannel(std::string &nick);
+		bool							isOperator(std::string &operatorName);
 		void							removeChannelPassword();
-		void							addToKicked(std::string &banned_name);
 
 };
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -2,13 +2,100 @@
 #include "../includes/Channel.hpp"
 
 Channel::Channel(std::string const &name) : _channel(name), _userLimit(10){
+
 	_clientList.clear();
-	_kickedUsers.clear();
+	// _kickedUsers.clear();
 	_operators.clear();
 }
 
 Channel::~Channel(){}
 
+std::map <std::string, Client>&	Channel::getClientList() { return _clientList; }
+std::vector<std::string>&		Channel::getChannelOperators() { return _operators; }
 const std::string&				Channel::getChannelName() { return _channel; }
+const std::string&				Channel::getChannelKey() { return _channelKey; }
 const std::string&				Channel::getTopic() { return _topic; }
 const std::string&				Channel::getMode() { return _mode; }
+const int&						Channel::getUserLimit() { return _userLimit; }
+
+void	Channel::addToChannel(Client &client)
+{
+	std::string nick = client.getNickName();
+
+	_clientList.emplace(nick, client);
+}
+
+void	Channel::removeFromChannel(std::string &nick)
+{
+	_clientList.erase(nick);
+}
+
+void	Channel::addOperator(std::string operatorName)
+{
+	_operators.push_back(operatorName);
+}
+
+void	Channel::removeOperator(std::string operatorName)
+{
+	std::vector<std::string>::iterator it = std::find(_operators.begin(), _operators.end(), operatorName);
+	_operators.erase(it);
+}
+
+void	Channel::setChannelKey(std::string password)
+{
+	_channelKey = password;
+}
+
+void	Channel::setTopic(std::string& newTopic)
+{
+	_topic = newTopic;
+}
+
+void	Channel::addMode(std::string const mode)
+{
+	if (_mode.empty() == true)
+		_mode = "+" + mode;
+	else
+		_mode += mode;
+}
+
+void	Channel::removeMode(std::string const mode)
+{
+	size_t pos = 0;
+
+	pos = _mode.find(mode);
+	if (pos != 0)
+		_mode.erase(pos, mode.length());
+}
+
+void	Channel::setUserLimit(int limit)
+{
+	_userLimit = limit;
+}
+
+bool	Channel::isAlreadyInChannel(std::string &nick)
+{
+	if (_clientList.size() == 0)
+		return(false);
+	std::map<std::string, Client>::iterator it = _clientList.find(nick);
+	if (it == _clientList.end())
+		return(false);
+	return(true);
+}
+
+bool	Channel::isOperator(std::string &operatorName)
+{
+	if (_operators.size() == 0)
+		return(false);
+	std::vector<std::string>::iterator it = std::find(_operators.begin(), _operators.end(), operatorName);
+	if (it == _operators.end())
+		return(false);
+	return(true);
+}
+
+void	Channel::removeChannelPassword()
+{
+	_channelKey.clear();
+}
+
+
