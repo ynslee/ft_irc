0440e6dea4ec1e07f7d3061992131e4175401d6a
ynslee <112563002+ynslee@users.noreply.github.com>
2024-02-21 14:33:46 +0200

    FI-45 checking if client exists already

--- srcs/commands/Join.cpp
+++ srcs/commands/Join.cpp
@@ -1,13 +1,33 @@
 #include "../../includes/Commands.hpp"
 #include "../../includes/Server.hpp"
 
-static std::string getChannelName(std::string channel)
+static std::string getChannelName(std::string channel, std::map<std::string, Channel*> &channels)
 {
 	std::size_t found = channel.find('#');
+	std::string channelName;
+
 	if (found != std::string::npos)
-		return (channel);
+		channelName = channel;
 	else
-		return ("#" + channel);
+		channelName = "#" + channel;
+	std::map<std::string, Channel*>::iterator it;
+	for (it=channels.begin(); it!=channels.end(); it++)
+	{
+		if (it->first == channelName)
+			return (channelName);
+	}
+	return (channelName);
+}
+
+static bool checkIfClientExists(std::map<const std::string, Client*> &clientList, std::string nick)
+{
+	std::map<const std::string, Client*>::iterator it;
+	for (it=clientList.begin(); it!=clientList.end(); it++)
+	{
+		if (it->first == nick)
+			return (true);
+	}
+	return (false);
 }
 
 static int joinExistingServerWithoutKey(std::map<std::string, Channel*> &channels, std::string channelName, Client *Client)
@@ -22,6 +42,8 @@ static int joinExistingServerWithoutKey(std::map<std::string, Channel*> &channel
 				send(Client->getClientFd(), ERR_BADCHANNELKEY(Client->getHostName(), channelName).c_str(), ERR_BADCHANNELKEY(Client->getHostName(), channelName).length(), 0);
 				return (-1);
 			}
+			if (checkIfClientExists(it->second->getClientList(), Client->getNickName()) == true)
+				return (0);
 			it->second->addToChannel(*Client);
 			return (0);
 		}
@@ -73,9 +95,9 @@ int cmdJoin(Message &msg, Client *Client, std::map<std::string, Channel*> &chann
 	}
 	if (channels.size() == 0)
 	{
-		channelName = getChannelName(msg.params[0]);
 		if(msg.params.size() <= 2)
 		{
+			channelName = getChannelName(msg.params[0], channels);
 			channels[channelName] = new Channel(channelName);
 			channels[channelName]->addToChannel(*Client);
 			if (msg.params.size() == 2)
