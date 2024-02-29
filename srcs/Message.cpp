998ba898dabc5b30e7ae8e3b9abdcb815e9dfeb0
Josefina Husso <106306928+hussojo@users.noreply.github.com>
2024-02-29 10:30:29 +0200

    Update Message.cpp

--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -41,7 +41,7 @@ Message::Message(std::string input)
 		{
 			std::string trail = token.substr(1);
 			std::string temp;
-			getline(iss, temp, '\n');
+			getline(iss, temp, '\r');
 			trail += temp;
 			this->trailing = trail;
 			std::cout << "TRAILING: " << trailing << std::endl;
