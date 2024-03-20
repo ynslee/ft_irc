4c35fcc84f2ac72533ac6d521251f1d8f5502a0e
jestebanpelaez18 <juanes.pelaez18@gmail.com>
2024-03-20 14:46:10 +0200

    fixed nick when we changes the nick of a user that is in a channel

--- srcs/commands/Nick.cpp
+++ srcs/commands/Nick.cpp
@@ -26,20 +26,34 @@
  * Example : /NICK nickname
  */
 
-static void changeNickinChannels(Client *client, Server &server)
+static void changeNickinChannels(Client *client, Server &server, std::string old_nick)
 {
-	//match clients channels to server map->first and modify clientlist and operator list in server to have the new nickname
-	std::vector<std::string>::iterator clientChannels;
-	for (clientChannels = client->getChannelsJoined().begin(); clientChannels != client->getChannelsJoined().end(); clientChannels++)
-	{
-		std::cout << "clientChannels: " << std::endl;
-		std::map<std::string, Channel*>::iterator serverChannels;
-		for (serverChannels = server.getChannels().begin(); serverChannels != server.getChannels().end(); serverChannels++)
-		{
-			std::cout << "serverChannels: " << std::endl;
-			// if (clientChannels == serverChannels)
-		}
-	}
+    std::vector<std::string>::iterator it;
+    for(it = client->getChannelsJoined().begin(); it < client->getChannelsJoined().end(); it++)
+    {
+        std::string channel_name = *it;
+        std::map<std::string, Channel*>::iterator channelIt = server.getChannels().find(channel_name);
+        if(channelIt != server.getChannels().end())
+        {
+            if(channelIt->second->isOperator(old_nick) ==  true)
+            {
+                channelIt->second->removeOperator(old_nick);
+                channelIt->second->addOperator(client->getNickName());
+            }
+            channelIt->second->getClientList().insert(std::make_pair(client->getNickName(), (client)));
+            channelIt->second->getClientList().erase(old_nick);
+        }
+    }
+    std::map<std::string, Channel*>::iterator iter2;
+    for(iter2 = server.getChannels().begin(); iter2 != server.getChannels().end(); iter2++)
+    {
+        std::vector<std::string>::iterator inviteIter = std::find(iter2->second->getInvitedList().begin(), iter2->second->getInvitedList().end(), old_nick);
+        if(inviteIter != iter2->second->getInvitedList().end())
+        {
+            iter2->second->getInvitedList().erase(inviteIter);
+            iter2->second->getInvitedList().push_back(client->getNickName());
+        }
+    }
 }
 
 int isValidnick(std::string new_nick)
@@ -95,9 +109,7 @@ int cmdNick(Message &msg, Client *Client, std::vector<std::string> &nick_names, 
         std::vector<std::string>::iterator it = std::find(nick_names.begin(),nick_names.end(), old_nick);
         nick_names.erase(it);
         nick_names.push_back(new_nick);
-		//operatorlist for cliets channels
-		changeNickinChannels(Client, server);
-		//invited list for all channels
+		changeNickinChannels(Client, server, old_nick);
         send(Client->getClientFd(), NICK_REPLY(old_nick,Client->getUserName(),Client->getIPaddress(), new_nick).c_str(), NICK_REPLY(old_nick,Client->getUserName(),Client->getIPaddress(), new_nick).length(),0);
         return(0);
     }
